74d420b76856fe2ef51a15996f22f742545ddb2d
Farooq Dad khan <farooqdaadkhan@gmail.com>
2024-09-05 19:33:17 +0500

    changes to home and contact

--- js/form-code.js
+++ js/form-code.js
@@ -1,24 +1,40 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxOv-sRrPq67Ribzln8u372JuYS4KbeXNglqnmqHHPSfswe5jnaohtC-aCps3Tabm_V/exec'
-
-const form = document.forms['contact-form']
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxOv-sRrPq67Ribzln8u372JuYS4KbeXNglqnmqHHPSfswe5jnaohtC-aCps3Tabm_V/exec';
+const form = document.forms['contact-form'];
+const notification = document.getElementById("notification-success");
+const notificationError = document.getElementById("notification-error");
+const modal = new bootstrap.Modal(document.getElementById('quoteForm'));
 
 form.addEventListener('submit', e => {
-  e.preventDefault()
-  document.getElementById("submit").innerHTML = "Submitting.."
+  e.preventDefault();
+  document.getElementById("submit").innerHTML = "Submitting...";
 
   fetch(scriptURL, { method: 'POST', body: new FormData(form) })
     .then(response => {
-      alert("Thank you! your form is submitted successfully.")
-    }).then(() => {
-      window.location.reload();
-      console.log("Form submitted successfully");
-      document.getElementById("submit").innerHTML = "Submit your request"
+      console.log('Success!', response);
 
+      // Trigger the slide-down notification
+      notification.classList.add('show');
+
+      // Hide the notification after 5 seconds
+      setTimeout(() => {
+        notification.classList.remove('show');
+      }, 5000);
+
+
+      form.reset();
+      modal.hide();
+
+      document.getElementById("submit").innerHTML = "Submit your request";
     })
     .catch(error => {
-      console.error('Error!', error.message)
-      document.getElementById("submit").innerHTML = "Submit your request"
-    }
+      // Trigger the slide-down notification
+      notificationError.classList.add('show');
 
-    )
-})
+      // Hide the notification after 5 seconds
+      setTimeout(() => {
+        notificationError.classList.remove('show');
+      }, 5000);
+      console.error('Error!', error.message);
+      document.getElementById("submit").innerHTML = "Submit your request";
+    });
+});
