db44b58a907de4b5fbc8e870ffbfa9fe6db803df
MUHAMMAD NABEEL NISAR <115373801+MuhammadNabeelNisarWorkspace@users.noreply.github.com>
2024-09-15 19:00:56 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { Testimonials } from "@/components/testimonials";
 import { CaseStudies } from "@/components/case-studies";
 import { Hero } from "@/components/hero";
+import WorkWithUs from "@/components/work-with-us";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
       <CaseStudies />
+      <WorkWithUs />
       <Testimonials />
     </>
   );
--- src/components/case-studies.tsx
+++ src/components/case-studies.tsx
@@ -2,14 +2,14 @@ import { CaseStudiesCard } from "./case-studies-card";
 
 export function CaseStudies() {
   return (
-    <section>
-      <div className="mx-auto max-w-2xl mb-10 sm:text-center">
+    <section className="mb-10">
+      <div className="mx-auto mb-10 max-w-2xl sm:text-center">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           Simple no-tricks pricing
         </h2>
         <p className="mt-6 text-lg leading-8 text-gray-600">
-          Distinctio et nulla eum soluta et neque labore quibusdam. Saepe et quasi iusto modi velit
-          ut non voluptas in. Explicabo id ut laborum.
+          Distinctio et nulla eum soluta et neque labore quibusdam. Saepe et
+          quasi iusto modi velit ut non voluptas in. Explicabo id ut laborum.
         </p>
       </div>
 
