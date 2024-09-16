144dabfe654e6f969e01e7b834e3e61ae98779f6
Farooq Dad khan <farooqdaadkhan@gmail.com>
2024-09-16 20:32:14 +0500

    Update case-studies-card.tsx

--- src/components/case-studies-card.tsx
+++ src/components/case-studies-card.tsx
@@ -1,20 +1,19 @@
 
-export function CaseStudiesCard({img}:{img: string}) {
+export function CaseStudiesCard({category, title, desc, img}:{category:string, title:string, desc:string, img: string}) {
   return (
-    <div className="overflow-hidden bg-white py-10">
+    <div className="overflow-hidden bg-white py-5">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
             <div className="lg:max-w-lg">
-              <h2 className="text-base font-semibold leading-7 text-primary">E commerce Website</h2>
+              <h2 className="text-base font-semibold leading-7 text-primary">{category}</h2>
               <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                Winter Sale 
+                {title}
               </p>
               <p className="mt-6 text-lg leading-8 text-gray-600">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit
-                perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                {desc}
               </p>
-              <button className="btn mt-5 text-lg">read more »</button>
+              {/* <button className="btn mt-5 text-lg">read more »</button> */}
             </div>
           </div>
           <img
