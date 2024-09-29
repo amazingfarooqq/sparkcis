4f8a3f9e11455499c576522080a0057b67081482
MUHAMMAD NABEEL NISAR <115373801+MuhammadNabeelNisarWorkspace@users.noreply.github.com>
2024-09-29 16:31:05 +0500

    update

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const testimonials = [
   {
@@ -8,7 +8,7 @@ const testimonials = [
     role: "Founder",
     company: "Landscape Ventures",
     commaColor: "orange",
-    img: ""
+    img: "",
   },
   {
     text: "I use almost daily - from quick copy-paste solutions for prototyping, to inspiration in my own designs. The prebuilt components allow for rapid prototyping, saving me hours in pixel perfect design time. Efficient, clean cut, and allround badass!",
@@ -29,53 +29,61 @@ const testimonials = [
     role: "Founder",
     commaColor: "orange",
   },
-]
+];
 export function Testimonials() {
   return (
     <>
-      <section aria-label="A word from our users" className="w-full relative h-full py-32 ">
-        <div aria-hidden="true" className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
+      <section
+        aria-label="A word from our users"
+        className="relative h-full w-full py-32"
+      >
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        >
           <div
             style={{
               clipPath:
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }} className="relative left-[calc(80%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#23b5b5] to-[#a0d3d3] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" />
+            }}
+            className="relative left-[calc(80%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#23b5b5] to-[#a0d3d3] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          />
         </div>
         <div
-          className="hidden absolute dark:block placeholder2 h-full w-full"
+          className="placeholder2 absolute hidden h-full w-full dark:block"
           data-large="https://tuk-cdn.s3.amazonaws.com/assets/gradients/faq_bg-gradient.png"
         >
           <img
             src="https://tuk-cdn.s3.amazonaws.com/assets/gradients/faq_bg-gradient.png"
-            className="img-small2 h-full w-full loaded"
+            className="img-small2 loaded h-full w-full"
             alt="gradient image"
           />
           <img
             src="https://tuk-cdn.s3.amazonaws.com/assets/gradients/faq_bg-gradient.png"
-            className="loaded absolute inset-0 w-full h-full max-h-full"
+            className="loaded absolute inset-0 h-full max-h-full w-full"
             alt="gradient image"
           />
         </div>
-        <h3 className="pb-1.5 lg:leading-tight md:leading-tight relative z-20 2xl:text-5xl md:text-5xl text-3xl font-extrabold leading-tight text-center wordfromclient t-f-c bg-clip-text text-primary">
+        <h3 className="wordfromclient t-f-c relative z-20 bg-clip-text pb-1.5 text-center text-3xl font-extrabold leading-tight text-primary md:text-5xl md:leading-tight lg:leading-tight 2xl:text-5xl">
           Over 1000+ happy customers
         </h3>
-        <div className="relative z-20 mx-auto container grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-8 xl:mt-[82px] mt-16 2xl:px-4 lg:px-10 px-4">
+        <div className="container relative z-20 mx-auto mt-16 grid gap-8 px-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 lg:px-10 xl:mt-[82px] xl:grid-cols-4 2xl:px-4">
           {testimonials.map((testimonial, index) => (
             <div key={index}>
-              <div className="testimonialSectionCard 2xl:h-[482px] border-4 border-gray-500 dark:border-opacity-10 border-opacity-20 rounded-2xl 2xl:p-6 px-4 py-6 h-full">
-                <div className="flex flex-col h-full justify-between">
+              <div className="testimonialSectionCard h-full rounded-2xl border-4 border-gray-500 border-opacity-20 px-4 py-6 dark:border-opacity-10 2xl:h-[482px] 2xl:p-6">
+                <div className="flex h-full flex-col justify-between">
                   <div>
                     <img
                       src={`https://tuk.dev/img/testimonials/${testimonial.commaColor}-comma.svg`}
                       alt="testimonial"
                     />
-                    <p className="2xl:text-lg text-base font-medium leading-7 mt-6 text-gray-700">
+                    <p className="mt-6 text-base font-medium leading-7 text-gray-700 2xl:text-lg">
                       {testimonial.text}
                     </p>
                   </div>
-                  <div className="mt-10 flex items-center ">
+                  <div className="mt-10 flex items-center">
                     <img
-                      className="w-10 h-10 rounded-full"
+                      className="h-10 w-10 rounded-full"
                       src="https://tuk.dev/img/profile-pic/CasduPlesis.png"
                       alt="customer profile image"
                     />
@@ -83,8 +91,9 @@ export function Testimonials() {
                       <p className="text-base font-extrabold leading-4 text-gray-600">
                         {testimonial.name}
                       </p>
-                      <p className="text-sm font-light leading-none text-gray-600 dark:text-white mt-2">
-                        <span>{testimonial.role}</span>{testimonial.company ? ` - ${testimonial.company}` : ''}
+                      <p className="mt-2 text-sm font-light leading-none text-gray-600 dark:text-white">
+                        <span>{testimonial.role}</span>
+                        {testimonial.company ? ` - ${testimonial.company}` : ""}
                       </p>
                     </div>
                   </div>
@@ -107,24 +116,29 @@ export function Testimonials() {
           />
         </div>
       </section>
-      <section aria-label="Get started for free"
-        className="py-12 pb-40 bg-"
-      >
-        <h3 className="text-4xl md:text-5xl font-bold mb-4 text-center">
-          Schedule a free <span className="text-primary">consultation <br /> with one </span>
+
+      <section aria-label="Get started for free" className="bg-primary py-32">
+        <h3 className="mb-4 text-center text-4xl font-bold text-white md:text-5xl">
+          Schedule a free{" "}
+          <span className="">
+            consultation <br /> with one{" "}
+          </span>
           of our experts.
         </h3>
-        <div className="flex flex-col items-center justify-center mt-16 md:w-auto w-full">
-
-
+        <div className="mt-7 flex w-full flex-col items-center justify-center md:w-auto">
           <Link href="/contact">
-            <Button className="text-xl px-10 py-3 h-auto " style={{ borderRadius: "10px" }}>Schedule now »</Button>
+            <Button
+              className="h-auto bg-white px-10 py-3 text-xl text-black hover:bg-slate-200 hover:text-black"
+              style={{ borderRadius: "10px" }}
+            >
+              Schedule now »
+            </Button>
           </Link>
-          <p className="lg:text-base text-xs leading-4 lg:mt-3 mt-2 text-gray-400">
+          <p className="mt-2 text-xs leading-4 text-white lg:mt-3 lg:text-base">
             Fix a free strategy call with our experts
           </p>
         </div>
       </section>
     </>
-  )
+  );
 }
