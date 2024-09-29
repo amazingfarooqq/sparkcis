2e6f300898303cd287407e7695e1174186c5e0f6
Farooq Dad khan <farooqdaadkhan@gmail.com>
2024-09-29 18:05:06 +0500

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -28,7 +28,7 @@ const formSchema = z.object({
   message: z.string().min(1, "Message is required"),
 });
 
-export default function ContactPage() {
+export default function ContactPagee() {
   const form = useForm<FormData>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -53,142 +53,138 @@ export default function ContactPage() {
   }
 
   return (
-    <div className="isolate mt-20 bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div
-        aria-hidden="true"
-        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-      >
-        <div
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#23b5b5] to-[#a0d3d3] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-        />
-      </div>
-      <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          Contact us
-        </h2>
-        <p className="mt-2 text-lg leading-8 text-gray-600">
+    <>
+      <div className=" ">
+        <div aria-hidden="true" className="mt-40 inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl"> </div>
+        <div className="mx-auto max-w-xl text-center">
+          <h2 className="text-6xl font-bold tracking-tight text-gray-900 ">
+            Lets talk
+          </h2>
+          <p className="mt-2 text-lg leading-8 text-gray-600">
           Enter your details and someone from our team will reach out to find a
-          time to connect with you.
-        </p>
+            time to connect with you.
+            </p>
+        </div>
+        <div className="py-10 flex items-center justify-center mx-2">
+          <div className="relative   ">
+            <div className="absolute inset-0 bg-primary rounded-lg transform -rotate-6 scale-100 z-0  hidden md:block"></div>
+            <div className="relative px-10 py-10 text-white bg-gray-800 border border-gray-600 rounded-lg shadow-xl overflow-hidden z-10  ">
+
+              <div className="relative">
+                <Form {...form}>
+                  <form onSubmit={form.handleSubmit(onSubmit)} className="mx-auto mt-16 w-[78vw] md:w-[58vw]  xl:w-[600px] sm:mt-20">
+                    <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2 w-full">
+                      <FormField
+                        control={form.control}
+                        name="firstName"
+                        render={({ field }) => (
+                          <FormItem className="sm:col-span-2">
+                            <FormLabel>First name</FormLabel>
+                            <FormControl>
+                              <Input {...field} className="border border-gray-500"/>
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      
+                      <FormField
+                        control={form.control}
+                        name="lastName"
+                        render={({ field }) => (
+                          <FormItem className="sm:col-span-2">
+                            <FormLabel>Last name</FormLabel>
+                            <FormControl>
+                              <Input {...field} className="border border-gray-500"/>
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                          <FormItem className="sm:col-span-2">
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                              <Input {...field} type="email" className="border border-gray-500" />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="phoneNumber"
+                        render={({ field }) => (
+                          <FormItem className="sm:col-span-2">
+                            <FormLabel>Phone number</FormLabel>
+                            <FormControl>
+                              <Input {...field} className="border border-gray-500"/>
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="company"
+                        render={({ field }) => (
+                          <FormItem className="sm:col-span-2">
+                            <FormLabel>Company</FormLabel>
+                            <FormControl>
+                              <Input {...field} className="border border-gray-500"/>
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="message"
+                        render={({ field }) => (
+                          <FormItem className="sm:col-span-2">
+                            <FormLabel>Message</FormLabel>
+                            <FormControl>
+                              <Textarea {...field} className="border border-gray-500"/>
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+
+                    {mutation.isError && (
+                      <Alert variant="destructive" className="mt-10">
+                        <AlertTitle>Error</AlertTitle>
+                        <AlertDescription>{mutation.error.message}</AlertDescription>
+                      </Alert>
+                    )}
+
+                    {mutation.isSuccess && (
+                      <Alert className="mt-10">
+                        <AlertTitle>Success</AlertTitle>
+                        <AlertDescription>Your message has been sent successfully!</AlertDescription>
+                      </Alert>
+                    )}
+
+                    <div className="mt-10">
+                      <Button type="submit" className="w-full" disabled={mutation.isPending}>
+                        {mutation.isPending ? "Submitting..." : "Let's talk"}
+                      </Button>
+                    </div>
+                  </form>
+                </Form>
+              </div>
+            </div>
+
+            <div></div>
+          </div>
+        </div>
+      <div className="h-[700px] bg-gray-900  -mt-36 md:-mt-48 lg:-mt-[500px]"></div>
+
       </div>
-
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="mx-auto mt-16 max-w-xl sm:mt-20"
-        >
-          <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-            <FormField
-              control={form.control}
-              name="firstName"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>First name</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="lastName"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Last name</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem className="sm:col-span-2">
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input {...field} type="email" />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="phoneNumber"
-              render={({ field }) => (
-                <FormItem className="sm:col-span-2">
-                  <FormLabel>Phone number</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="company"
-              render={({ field }) => (
-                <FormItem className="sm:col-span-2">
-                  <FormLabel>Company</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="message"
-              render={({ field }) => (
-                <FormItem className="sm:col-span-2">
-                  <FormLabel>Message</FormLabel>
-                  <FormControl>
-                    <Textarea {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-
-          {mutation.isError && (
-            <Alert variant="destructive" className="mt-10">
-              <AlertTitle>Error</AlertTitle>
-              <AlertDescription>{mutation.error.message}</AlertDescription>
-            </Alert>
-          )}
-
-          {mutation.isSuccess && (
-            <Alert className="mt-10">
-              <AlertTitle>Success</AlertTitle>
-              <AlertDescription>
-                Your message has been sent successfully!
-              </AlertDescription>
-            </Alert>
-          )}
-
-          <div className="mt-10">
-            <Button
-              type="submit"
-              className="w-full"
-              disabled={mutation.isPending}
-            >
-              {mutation.isPending ? "Submitting..." : "Let's talk"}
-            </Button>
-          </div>
-        </form>
-      </Form>
-    </div>
+    </>
   );
 }
