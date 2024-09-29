fa7b4a1b7fefb1847e5ae00afde24bc2ef734dbc
Farooq Dad khan <farooqdaadkhan@gmail.com>
2024-09-29 18:05:28 +0500

    Update web-dev-service.tsx

--- src/components/web-dev-service.tsx
+++ src/components/web-dev-service.tsx
@@ -24,7 +24,7 @@ export function WebDevService() {
         <div className="bg-white py-24 sm:py-32">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto max-w-2xl lg:mx-0">
-                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">CUSTOM WEB APPLICATION DEVELOPMENT                    </h2>
+                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">CUSTOM WEB APPLICATION DEVELOPMENT                    </h2>
                     <p className="mt-2 text-lg leading-8 text-gray-600">
                         High-performing, intuitive and secure web solutions that support business processes and serve users globally.
                     </p>
@@ -32,7 +32,10 @@ export function WebDevService() {
                 <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                     {posts.map((post) => (
                         <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
+
                             <div className="group relative">
+                                
+
                                 <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
                                     <div>
                                         <span className="absolute inset-0" />
